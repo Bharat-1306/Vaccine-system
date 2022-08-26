45cd7f2d15e74a1c6720dd7122e102f9079c4a33
Bharat Kumar <vabharatkumar@deloitte.com>
2022-08-26 13:39:16 +0530

    Case2

--- src/Components/Vaccine/Administer/Administer.js
+++ src/Components/Vaccine/Administer/Administer.js
@@ -8,16 +8,37 @@ const Administer = () => {
   const inputPatientNameRef = useRef();
   const inputDateRef = useRef();
   const inputDateAdministeredRef = useRef();
+  const inputVaccinationRef = useRef();
+  const inputBrandNameRef = useRef();
+  const inputHospitalRef = useRef();
+  const [x, Sx] = useState();
+  const [patient, Setpatient] = useState();
+
+  const saveDataHandler = (event) => {
+    event.preventDefault();
+    console.log(inputPatientNameRef.current.value);
+    axios
+      .post("http://localhost:9000/vaccination-details", {
+        Name: inputPatientNameRef.current.value,
+        Date_of_Birth: inputDateRef.current.value,
+        Date_Administered: inputDateAdministeredRef.current.value,
+        Brand_name: inputBrandNameRef.current.value,
+        Given_at: inputHospitalRef.current.value,
+        Vaccination: inputVaccinationRef.current.value,
+      })
+      .then((Response) => {
+        // setUsers(Response.data);
+        navigate("/Home");
+      })
+      .catch(console.error());
+  };
+
   const [state, setState] = useState({
     values: [],
   });
   const cancelDataHandler = (event) => {
     event.preventDefault();
     navigate("/Home");
-  };
-
-  const saveDataHandler = (event) => {
-    event.preventDefault();
   };
 
   useEffect(() => {
@@ -30,16 +51,26 @@ const Administer = () => {
             values: [...res.data],
           });
           console.log(state);
+          // Setpatient()
           return res.json;
         })
         .then((json) => {});
     }
   }, []);
+  const changeHandler = () => {
+    console.log(inputPatientNameRef.current.value);
+    Sx(state.values[inputPatientNameRef.current.value - 1].Date);
+  };
   return (
     <div className="row">
       <div className="drop-down col-6 offset-3 border">
         <p>Select the Name of the Patient :</p>
-        <select className="form-control" ref={inputPatientNameRef}>
+        <select
+          className="form-control"
+          ref={inputPatientNameRef}
+          onChange={changeHandler}
+        >
+          <option value="0">select</option>
           {state.values.map((obj) => {
             console.log(obj);
 
@@ -52,11 +83,19 @@ const Administer = () => {
         </select>
         <br />
         <label>Date of Birth:</label>
-        <input type="date" className="form-control" ref={inputDateRef}></input>
+        {x === 0 ? (
+          <input type="date" className="form-control" />
+        ) : (
+          <input type="date" className="form-control" value={x} />
+        )}
 
         <br />
         <h6>Vaccination:</h6>
-        <select htmlFor="vaccination" className="form-control">
+        <select
+          htmlFor="vaccination"
+          className="form-control"
+          ref={inputVaccinationRef}
+        >
           <option>Vaccination Dose 1</option>
           <option>Vaccination Dose 2</option>
           <option>Vaccination Dose 3</option>
@@ -65,17 +104,24 @@ const Administer = () => {
         <label htmlFor="date">Date Administered:</label>
         <input
           type="date"
-          max={"2022 - 08 - 26"}
+          max="2022-08-26"
           className="form-control"
           ref={inputDateAdministeredRef}
-          // max="08 / 26 / 2022"
         ></input>
         <br />
         <label htmlFor="Brand-name">Brand-Name</label>
-        <input className="form-control" type="text"></input>
+        <input
+          className="form-control"
+          type="text"
+          ref={inputBrandNameRef}
+        ></input>
         <br />
         <label htmlFor="Given-at">Name of the Hospital:</label>
-        <input className="form-control" type="text"></input>
+        <input
+          className="form-control"
+          type="text"
+          ref={inputHospitalRef}
+        ></input>
 
         <br />
 
@@ -83,7 +129,7 @@ const Administer = () => {
           <div className="col-12">
             <button
               className="btn btn-primary  col-4"
-              onSubmit={saveDataHandler}
+              onClick={saveDataHandler}
             >
               Save
             </button>
